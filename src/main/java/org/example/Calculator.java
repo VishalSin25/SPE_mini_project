874d0b438f86f68b6aa258490001098676413506
VishalSin25 <rvishal251996@gmail.com>
2023-03-21 22:38:09 +0530

    final commit

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -1,61 +1,139 @@
 package org.example;
-import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Calculator {
-
     private static final Logger logger = LogManager.getLogger(Calculator.class);
+//    System.setProperty("log4j.configurationFile","./src/main/resources/log4j2.xml");
 
-
-    public Calculator() {
-    }
-
-    public static void main(String[] args)
-    {
-        Calculator calculator = new Calculator();
-        Scanner scanner = new Scanner(System.in);
-        double number1, number2;
+    public static void main(String []args) {
+        double a, b;
+        int choice=0;
         do {
-            System.out.println("Scientific Calculator using DevOps. \n Choose operation:");
-            System.out.print("1. Addition\n"+ "2. Exit\nEnter your choice: ");
-            int choice;
-            try {
-                choice = scanner.nextInt();
-            } catch (InputMismatchException error) {
-                return;
+            System.out.println("\n\n\n------welcome to scientific calculator-------\n");
+            logger.info("A user entered !");
+
+            System.out.println("Scientific Calculator");
+            System.out.println("---------------------");
+            System.out.println("1. Factorial function ");
+            System.out.println("2. Natural logarithm");
+            System.out.println("3. Square root");
+            System.out.println("4. Power function ");
+            System.out.println("5. Exit");
+
+            System.out.print("Enter your choice: ");
+
+            Scanner sc = new Scanner(System.in);
+             choice = sc.nextInt();
+
+            long res1 = 0;
+            double res2 = 0.0;
+            if(choice<1 || choice>5)
+            {
+                System.out.println("invalid choice!\n\n");
+                continue;
             }
             switch (choice) {
                 case 1:
-                    // Factorial
-                    System.out.print("Enter first number : ");
-                    number1 = scanner.nextDouble();
-                    System.out.print("Enter second number : ");
-                    number2 = scanner.nextDouble();
-                    System.out.println("Addition of " + number1 +"and"+ number2 +" is : " + calculator.Addition(number1, number2));
-                    System.out.println("\n");
-
+                    System.out.println("enter a number :");
+                    a = sc.nextDouble();
+                    try {
+                        res1 = fact(a);
+                        System.out.println("Factorial of "+a+" is = "+res1);
+                    }
+                    catch (ArithmeticException e)
+                    {
+                        System.out.println(e.getMessage());
+                    }
+                    break;
+                case 2:
+                    System.out.println("enter a number :");
+                    a = sc.nextDouble();
+                    try {
+                        res2 = log(a);
+                        System.out.println("Natural logarithm of "+a+" is = "+res2);
+                    }
+                    catch (ArithmeticException e)
+                    {
+                        System.out.println(e.getMessage());
+                    }
+                    break;
+                case 3:
+                    System.out.println("enter a number :");
+                    a = sc.nextDouble();
+                    try {
+                        res2 = sqrt(a);
+                        System.out.println("Square root of "+a+" is = "+res2);
+                    }
+                    catch (ArithmeticException e) {
+                        System.out.println(e.getMessage());
+                    }
+                    break;
+                case 4:
+                    System.out.println("enter 2 numbers :");
+                    a=sc.nextDouble();
+                    b=sc.nextDouble();
+                    System.out.println("Power of "+a+"is = "+pow(a,b));
+                    break;
+                case 5:
+                    logger.info("Exit");
+                    System.out.println("Exiting...");
                     break;
                 default:
-                    System.out.println("Exiting....");
-                    return;
-
-
+                    logger.info("Invalid choice");
+                    System.out.println("Invalid choice. Please try again.");
             }
-
-        }while(true);
-
-
-
-
+//            if(choice!=5)
+//            {
+//                System.out.println("Exiting application !");
+//                break;
+//            }
+        }while(choice!=5);
     }
-    public double Addition(double number1,double number2) {
-        logger.info("[POWER - " + number1 + " RAISED TO] " + number2);
-        double result= number1+number2;
-        logger.info("[RESULT - POWER] - " + result);
+
+    public static long fact(double a)
+    {
+        if(a<0)
+        { logger.error("Factorial of number less than 0 cant be found");
+
+            throw new ArithmeticException("Factorial of number less than 0 cant be found");}
+
+        long result = 1;
+        for (int factor = 2; factor <= a; factor++) {
+            result *= factor;
+        }
+        logger.info("Factorial of "+a);
+        logger.info("!"+a+" = "+result);
+
         return result;
     }
+    public static double pow(double a, double b)
+    {
+        logger.info(a+" raised to power "+b);
+        logger.info(a+"^"+b+" = "+Math.pow(a,b));
+        return Math.pow(a,b);
+    }
+    public static double log(double a)
+    {
+        if(a<=0)
+        { logger.error("Log of number less than equal to 0 cant be found");
+            throw new ArithmeticException("Log of number less than 0 cant be found");
+        }
+        logger.info("Natural logarithm of"+a);
+        logger.info("log("+a+") = "+Math.log(a));
+        return Math.log(a);
+    }
+    public static double sqrt(double a)
+    {
+        if(a<0)
+        { logger.error("Square root of number less than 0 cant be found");
 
+            throw new ArithmeticException("Square root of number less than 0 cant be found");
+        }
+        logger.info("Square root of "+a+" = "+Math.sqrt(a));
+        return Math.sqrt(a);
+    }
 }
--- src/test/java/org/example/CalculatorTest.java
+++ src/test/java/org/example/CalculatorTest.java
@@ -1,26 +1,67 @@
 package org.example;
-import static junit.framework.Assert.assertEquals;
-import static org.junit.Assert.*;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
-
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 public class CalculatorTest {
 
-    Calculator calculator=new Calculator();
+    Calculator cal = new Calculator();
     private static final double DELTA = 1e-15;
+    @Test
+    public void factorialTruePositiveTest() {
+        assertEquals("Factorial for true positive", 3628800.0, cal.fact(10.0), DELTA);
+    }
+    @Test
+    public void factorialFalsePositiveTest() {
+        assertNotEquals("Factorial for false positive",20.0, cal.fact(5.0), DELTA);
+    }
+
+    @Test(expected = ArithmeticException.class)
+    public void factExcept()
+    {
+        cal.fact(-1);
+    }
 
     @Test
-    public void AdditionTruePositive(){
-        assertEquals("Finding Addition of numbers for True Positive", 13, calculator.Addition(6,7),DELTA);
+    public void powTruePositiveTest() {
+        assertEquals("Power for true positive",100.0, cal.pow(10.0, 2.0), DELTA);
     }
 
     @Test
-    public void AdditionFalsePositive(){
-        assertNotEquals("Finding Addition  of  numbers for False Positive", 5, calculator.Addition(5,5),DELTA);
-
+    public void powFalsePositiveTest() {
+        assertNotEquals("Power for false positive",20.0, cal.pow(10.0, 2.0), DELTA);
     }
 
-    private void assertNotEquals(String s, int i, double addition, double delta) {
+    @Test
+    public void sqrtTruePositiveTest() {
+        assertEquals("Square root for true positive",5.0, cal.sqrt(25.0), DELTA);
     }
+
+    @Test
+    public void sqrtFalsePositiveTest() {
+        assertNotEquals("Square root for false positive",5.0, cal.sqrt(80.0), DELTA);
+    }
+    @Test(expected = ArithmeticException.class)
+    public void sqrtExcept()
+    {
+        cal.sqrt(-1);
+    }
+
+    @Test
+    public void logTruePositiveTest() {
+        assertEquals("Natural logarithm for true positive (integer)",3.4011973816621555, cal.log(30.0), DELTA);
+        assertEquals("Natural logarithm for true positive (double)",2.3513752571634776, cal.log(10.5), DELTA);
+    }
+    @Test
+    public void logFalsePositiveTest() {
+        assertNotEquals("Natural logarithm for false positive (integer)",5.0, cal.log(50.0), DELTA);
+        assertNotEquals("Natural logarithm for false positive (double)",5.0213, cal.log(10.5), DELTA);
+    }
+    @Test(expected = ArithmeticException.class)
+    public void logExcept()
+    {
+        cal.log(-1);
+    }
+
 }
